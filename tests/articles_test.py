3d7d1b67f06cd2e0aa63390ba55aa76f9757bc4b
ludwig <murimiludwig@gmail.com>
2022-05-04 08:03:18 +0300

    this will modify the articles test

--- tests/articles_test.py
+++ tests/articles_test.py
@@ -3,7 +3,7 @@ from app.models import Articles
 
 class ArticlesTest(unittest.TestCase):
     def setUp(self):
-        self.new_article = Articles("Vox.com","Riley","Hurricane Dorian","The storm has left at least 43 dead","http//www.vox.com","https://www.thehindu.com/static/theme/default/base/img/og-image.jpg","2019-09-07T19:41:51Z")
+        self.new_article = Articles("Vox.com","Dylan Matthew","Economic Strand","Have we been thinking about economic growth all wrong?","http//www.vox.com","https://www.vox.com/future-perfect/23048981/economic-growth-productivity-philippon","2022-04-30 T22:41:51Z")
 
     def instance_test(self):
         self.assertTrue(isinstance(self.new_article,Articles))
